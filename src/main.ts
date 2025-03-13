60a628a2cb83e4d879a5102561d824fe80b1f792
Bilal Ezzaim <bilalzaim499@gmail.com>
2025-03-13 11:15:39 +0000

    Finalisation du déploiement continu 🚀

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://moride.vercel.app/'],
     methods: 'GET,POST,PUT,DELETE,PATCH',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true, // This should be true only if cookies or credentials are sent
@@ -23,7 +23,7 @@ async function bootstrap() {
   app.setGlobalPrefix('/api/v1');
   app.useWebSocketAdapter(new IoAdapter(app));
 
-  await app.listen(3000); 
+  await app.listen(3000);
   console.log(`🚀 Server running on http://localhost:3000`);
 }
 bootstrap();
